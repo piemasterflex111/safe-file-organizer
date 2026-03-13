d4ff50a5f613ebdcb75afd35af39e97aa23c187c
Payam <payamadloo@gmail.com>
2026-03-12 19:06:54 -0700

    update scanner

--- src/file_tool/scanner.py
+++ src/file_tool/scanner.py
@@ -12,19 +12,13 @@ def list_files(p):
                 skip.append(item.name)
                 continue
             
-
-
-  
-                
             if item.suffix.lower() in ALLOWED_EXTENSIONS:
                     
                     accept.append(item.name)
             else:
                     skip.append(item.name)
                     
-
     return accept,skip
-
 
 if __name__=="__main__":
       acceptable,skippable = list_files(p)
