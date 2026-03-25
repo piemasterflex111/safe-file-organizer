c9e18a519118f7623b0b3c48edaaeb0d570d3d44
Payam <payamadloo@gmail.com>
2026-03-24 21:24:34 -0700

    extended logic to rename files in subfolder directories

--- src/file_tool/scanner.py
+++ src/file_tool/scanner.py
@@ -1,12 +1,23 @@
 from config import ALLOWED_EXTENSIONS
 from config import INBOX_DIR
-from normalize_script import normalize_filename
+from normalize import normalize_filename
 
 def rename_files(INBOX_DIR):
     files = []
     for item in INBOX_DIR.iterdir():
-            if not item.is_file():
-                continue
+            if not item.is_dir():
+                 continue
+            for sub_item in item.iterdir():
+                if sub_item.is_file() and sub_item.suffix.lower() in ALLOWED_EXTENSIONS:
+                    old_name = sub_item.name
+                    new_name = normalize_filename(old_name)
+                    if old_name == new_name:
+                        continue
+                    new_path = sub_item.with_name(new_name)
+                    if new_path.exists():
+                        continue    
+                    new_path = sub_item.rename(new_path)
+                    files.append((old_name, new_name))
             if item.suffix.lower() in ALLOWED_EXTENSIONS:        
                     old_name = item.name
                     new_name = normalize_filename(old_name)
@@ -16,10 +27,10 @@ def rename_files(INBOX_DIR):
                     if new_path.exists():
                         continue    
                     new_path = item.rename(new_path)
-                    print(f"Renamed: {old_name} -> {new_name}")
-                    files.append((old_name, new_name))             
+                    files.append((old_name, new_name))
     return files
 if __name__ == "__main__":
+    print(f"Scanning directory: {INBOX_DIR}")
     file_list = rename_files(INBOX_DIR)
     for item in file_list:
         print(f"OLD FILES: {item[0]}\nNEW FILES: {item[1]}\n")
